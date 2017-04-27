197e866ae09c54103ba73b3ce59fee767e88e0aa
Tayon Miranda <dev@tayron.com.br>
2017-04-27 10:39:01 -0300

    UPDATE 	- Atualizando uso do php-di para trabalhar com interface

--- src/config/injection/config.php
+++ src/config/injection/config.php
@@ -1,12 +1,14 @@
 <?php
 
 return [
-    // ...
+    // Pessoa Controller
     PessoaController::class => \DI\object('MyApplication\controller\PessoaController')
         ->constructor(\DI\get('MyApplication\model\PessoaModel')),
     
+   // Pessoa Model
    MyApplication\model\PessoaModel::class => \DI\object('MyApplication\model\PessoaModel')
         ->constructor(\DI\get('MyApplication\model\Model')),
 
+   // Model
    MyApplication\model\Model::class => \DI\object('MyApplication\model\Model'),    
 ];
